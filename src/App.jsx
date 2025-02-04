02e1e9a21dd176f1ec78d365e7f5d76fd85ef379
Yash Agrahari <yashagrahari456@gmail.com>
2025-02-05 03:43:20 +0530

    Game, Board and Cell Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import './App.css'
+
+const initialBoard = () => Array(9).fill(null);
 
 function App() {
 
+  const [board, useBoard] = useState(initialBoard);
+
   return (
-    <>
-      Tic Tac Toe
-    </>
+    <div className='game'>
+      <div className="status">
+        Player X Turn
+        <button className='reset-button'>Reset Game</button>
+      </div>
+      <div className="board">
+        {board.map((_,index) => {
+          return <button className='cell' key={index}>X</button>
+        })}
+      </div>
+    </div>
   )
 }
 
